6a2c396e93d7cf73ce0f3876dc6953b1cafe21da
Saurabh <saurabhdaalia98@gmail.com>
2017-06-16 04:36:34 +0530

    bugs

--- app.py
+++ app.py
@@ -99,7 +99,7 @@ def location_data(lat,lng,type):
     final_url= base_url+location + "rankby=distance&types=" + type+ "&sensor=false&key="+ place_api
 
     #r=requests.get("https://maps.googleapis.com/maps/api/place/search/json?location=31.6607795,74.8214579&rankby=distance&types=police&sensor=false&key="+place_api)
-    r=request.get(final_url)
+    r=requests.get(final_url)
     #print r.content
     #d = ast.literal_eval(r)
 
