3f2d0e2a7e04f565db717b1f317e8cb4887dc112
Saurabh <saurabhdaalia98@gmail.com>
2017-06-16 03:21:11 +0530

    App file upadated

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import os, sys
-
-from flask import Flask, request
+from flask import Flask, request,  jsonify
 from pymessenger import Bot
-
+import requests
+import json
 
 app= Flask(__name__)
 
@@ -83,4 +83,52 @@ def log(msg):
     sys.stdout.flush()	
 	  
 if __name__ == "__main__":
+    app.run(debug=True, port =80)
+
+	
+#=======================================================#	
+	
+	
+place_api="AIzaSyDT0P7r0ezcoeNvotC_J2AwviQnCCfj0Oo"
+
+
+def location_data():
+    type="police"
+
+    r=requests.get("https://maps.googleapis.com/maps/api/place/search/json?location=31.6607795,74.8214579&rankby=distance&types=police&sensor=false&key="+place_api)
+    #print r.content
+    #d = ast.literal_eval(r)
+
+    result = json.loads(r.text)
+
+    data=dict(result)
+
+    #pprint.pprint(data)
+
+    result=data["results"]
+    final_data=dict()
+
+    final_data["data"]=list()
+
+
+    for item in result:
+        print "Name" , item["name"]
+        print "Address" ,item["vicinity"]
+        print "Co-ordinates", item["geometry"]["location"]["lat"], item["geometry"]["location"]["lng"] 
+        
+        d= {}
+        d["name"]=item["name"]
+        d["address"]=item["vicinity"]
+        d["lat"]=item["geometry"]["location"]["lat"]
+        d["lng"]=item["geometry"]["location"]["lng"]
+        final_data["data"].append(d)
+    return final_data
+
+@app.route('/api/sbcon/', ['GET'])
+def data():
+    return jsonify(location_data())
+   
+
+if __name__ == "__main__":
     app.run(debug=True, port =80)		
+	
